03555226ee3a6ad97c31a89285c39de1ebc1e491
Guilherme Esmero <guiesmero14@gmail.com>
2025-08-08 20:25:56 -0300

    Adicionar arquivos de projeto.

---
+++ IHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esmero.Mediator.Abstractions
+{
+    public interface IHandler<in TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        Task<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken = default);
+    }
+}
